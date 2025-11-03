2d4546c827009e124dac610dc8f61e8c5982ee6d
Gabriel Silvio da Silva Santos <silvio.apad@gmail.com>
2025-11-03 12:45:58 -0400

    Implementa navegação por drawer e atualiza mensagem de boas-vindas.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,22 @@
-import { Stack } from "expo-router";
+import { Drawer } from "expo-router/drawer";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Drawer
+        screenOptions={{
+          headerStyle: { backgroundColor: "#FFF" },
+        }}
+      >
+        <Drawer.Screen
+          name="index"
+          options={{
+            drawerLabel: "Página Inicial",
+            title: "Página Inicial",
+          }}
+        />
+      </Drawer>
+    </GestureHandlerRootView>
+  );
 }
